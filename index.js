d415bd2a0ffa020cb4213a9afcaf7cb5c18ac7e6
Mohana Bansal <mohanabansal10@gmail.com>
2020-01-13 21:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   let result = [];
+  let i = 0;
   for(let fact of facts) {
     result.push(fact+"!!!")
   }
   return result;
 }
+
+function iLoveTheBeatles(num){
+  let result = [];
+}
+
