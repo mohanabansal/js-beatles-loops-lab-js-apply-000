8dc0a08e6bdf6a0bb5fd3c084e20ed1a568d07a4
Mohana Bansal <mohanabansal10@gmail.com>
2020-01-13 21:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   let result = [];
   for(let i in musicians){
-    result.push(musicians[i]+ " plays " + instruments[i[]])
+    result.push(musicians[i]+ " plays " + instruments[i]);
   }
+  return result;
 }
